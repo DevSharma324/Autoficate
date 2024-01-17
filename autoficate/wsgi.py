64d7399a6f2ef6f8824ec70355d9aaf0890212f1
Dev-Sharma <sharma.dev.work@gmail.com>
2024-01-17 16:04:09 +0530

    Add WhiteNoise setup

--- autoficate/wsgi.py
+++ autoficate/wsgi.py
@@ -8,10 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoficate.settings')
 
+application = get_wsgi_application()
+
+# Specify the correct path to STATIC_ROOT
+static_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles')
+
 # `app` required for Vercel deployment
-app = get_wsgi_application()
+app = WhiteNoise(application, root=static_root)
