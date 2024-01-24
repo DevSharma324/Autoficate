ef1d437ab6f171bd32eafa9dbd6bf618cdbf7766
Dev-Sharma <sharma.dev.work@gmail.com>
2024-01-24 21:25:44 +0530

    Testing whitenoise root

--- autoficate/wsgi.py
+++ autoficate/wsgi.py
@@ -23,6 +23,8 @@ elapsed_time = end_time - start_time
 print(
     f"\nWSGI took {elapsed_time:.6f} seconds to execute.\n"
 )
+list_dirs_1 = lambda p='.': [d for d in os.listdir(p) if os.path.isdir(os.path.join(p, d))]; print(list_dirs(str(settings.STATIC_ROOT)))
+list_dirs_2 = lambda p='.': [d for d in os.listdir(p) if os.path.isdir(os.path.join(p, d))]; print(list_dirs(str(static_root)))
 
-print(settings.STATIC_ROOT)
-print(static_root)
+print("Settings " + settings.STATIC_ROOT + " - "  + str(list_dirs_1))
+print("Custom " + static_root + " - "  + str(list_dirs_2))
