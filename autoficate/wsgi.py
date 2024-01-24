1bf4b4cb8d13403492c8f6d5a9fb83b31c5ecd8d
Dev-Sharma <sharma.dev.work@gmail.com>
2024-01-24 20:13:28 +0530

    Update whitenoise config

--- autoficate/wsgi.py
+++ autoficate/wsgi.py
@@ -1,22 +1,25 @@
-"""
-WSGI config for autoficate project.
-
-It exposes the WSGI callable as a module-level variable named ``app``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
+import time
 
 import os
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoficate.settings')
 
-app = get_wsgi_application()
+start_time = time.time()
 
+
+application = get_wsgi_application()
 # Specify the correct path to STATIC_ROOT
-static_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles')
+static_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), settings.STATIC_ROOT)
 
 # `app` required for Vercel deployment
-# app = WhiteNoise(application, root=static_root)
+app = WhiteNoise(application, root=static_root)
+
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(
+    f"\nWSGI took {elapsed_time:.6f} seconds to execute.\n"
+)
