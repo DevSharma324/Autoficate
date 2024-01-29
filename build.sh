b61045ed16665d6fa24e46d2f56d7e4d6f5dbd97
Dev-Sharma <sharma.dev.work@gmail.com>
2024-01-29 19:41:09 +0530

    Update build config for render

--- build.sh
+++ build.sh
@@ -3,14 +3,8 @@
 # Install dependencies
 pipenv install --dev
 
-# Check for pending migrations
-python manage.py makemigrations --dry-run --check > /dev/null 2>&1
-PENDING_MIGRATIONS=$?
-
-# Apply migrations if there are pending changes
-if [ $PENDING_MIGRATIONS -eq 0 ]; then
-    python manage.py migrate
-fi
+# Activate Virtual Environment
+pipenv shell
 
 # Collect static files
 python manage.py collectstatic --noinput
